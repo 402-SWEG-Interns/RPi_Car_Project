fd6ba804529b049f7a4665514c14af3e064e3867
Engineral <lbraff03@gmail.com>
2023-06-29 12:52:59 -0400

    Fixed rotation values; implemented infrared while-loop

--- Code/Server/SWEGYweek1.py
+++ Code/Server/SWEGYweek1.py
@@ -1,53 +1,63 @@
-# This file contains the pathfinding code that utilizes both the Motor.py and the Line_Tracking.py files
+# This file contains the week1 code for the Freenove Car. It utilizes the Motor.py, Line_Tracking.py, and Led.py files. As for the obstacle avoidance code, that has yet to be completed within this file*.
+
+# *update when this is false.
+
+# Import Library
 import time
 import RPi.GPIO as GPIO
 from Motor import *
 from Line_Tracking import *
-from Led import *
+# from Led import *
 
 PWM=Motor()
-infrared=Line_Tracking()
+IR=Line_Tracking()
 
 # Wheels
-try:
-    PWM.setMotorModel(1000,0,0,0)
-    time.sleep(1)
-    PWM.setMotorModel(0,1000,0,0)
-    time.sleep(1)
-    PWM.setMotorModel(0,0,1000,0)
-    time.sleep(1)
-    PWM.setMotorModel(0,0,0,1000)
-    time.sleep(1)
-    PWM.setMotorModel(0,0,0,0)
-except KeyboardInterrupt:
-    PWM.setMotorModel(0,0,0,0)
-    Print("Program ended")
+# PWM.setMotorModel(Motor1, Motor2, Motor4, Motor3)   --- DO NOT UNCOMMENT ---
+# Motor1, Motor2, Motor4, Motor3   =   Front Left, Back Left, Front Right, Back Right
 
-# def completeTrack():
-#     incomplete = True
-#     while (incomplete):
-#         """URGENT
-#         Insert Async Coding? Or create three while loops: the outermost for the track completion, the middlemost for checking Infrared status, and the innermost for determining car action based on Infrared status."""
-#         while infra == False:
-#             try:
-#                 move("Forward")
-#                 print("The car is moving forward")
-#             except KeyboardInterrupt:
-#                 move("Stop")
-#                 print("\nEnd of program")
+def completeTrack():
+    manualIncomplete = True
+    while (manualIncomplete == True):
+        """URGENT
+        Insert Async Coding? Or create three while loops: the outermost for the track completion, the middlemost for checking Infrared status, and the innermost for determining car action based on Infrared status.
+        VERDICT
+        Three While Loops"""
 
-# def move(direction):
-#     if direction == "Forward":
-#         PWM.setMotorModel(1000,1000,1000,1000)
-#     elif direction == "Backward":
-#         PWM.setMotorModel(-1000,-1000,-1000,-1000)
-#     elif direction == "Left":
-#         PWM.setMotorModel(-1500,-1500,2000,2000)
-#         time.sleep(1)
-#         PWM.setMotorModel(0,0,0,0)
-#     elif direction == "Right":
-#         PWM.setMotorModel(2000,2000,-1500,-1500)
-#         time.sleep(1)
-#         PWM.setMotorModel(0,0,0,0)
-#     elif direction == "Stop":
-#         PWM.setMotorModel(0,0,0,0)
+        infrared = False
+        while (infrared == False):
+            try:
+                move("Forward")
+                print("The car is moving forward")
+            except KeyboardInterrupt:
+                move("Stop")
+                print("\nEnd of program")
+
+def move(direction):
+    try:
+        if   direction == "Forward":
+            PWM.setMotorModel(2000,2000,2000,2000)
+            print("Moving forward")
+            time.sleep(5)
+            stop()
+        elif direction == "Backward":
+            PWM.setMotorModel(-2000,-2000,-2000,-2000)
+            print("Moving backward")
+            time.sleep(5)
+            stop()
+        elif direction == "Right":
+            PWM.setMotorModel(2000,2000,-2000,-2000)
+            print("Turning right")
+            time.sleep(0.75)
+            stop()
+        elif direction == "Left":
+            PWM.setMotorModel(-2000,-2000,2000,2000)
+            print("Turning left")
+            time.sleep(0.75)
+            stop()
+    except KeyboardInterrupt:
+        stop()
+        print("\nProgram has ended")
+
+def stop():
+    PWM.setMotorModel(0,0,0,0)
