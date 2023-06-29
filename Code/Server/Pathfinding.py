5c54b4df7d0c3198eb111e8e13d51383c8ffd2fa
Engineral <lbraff03@gmail.com>
2023-06-29 08:15:12 -0400

    Imported classes and provided references

--- Code/Server/Pathfinding.py
+++ Code/Server/Pathfinding.py
@@ -1,3 +1,6 @@
 # This file contains the pathfinding code that utilizes both the Motor.py and the Line_Tracking.py files
 from Motor import *
 from Line_Tracking import *
+
+PWM=Motor()
+infrared=Line_Tracking()
