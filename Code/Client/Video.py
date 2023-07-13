53b8670cfcabd15f5bff51f2227c9b640fc2160d
Engineral <lbraff03@gmail.com>
2023-07-13 16:34:05 -0400

    Fine-tuning the TCP-thread process

--- Code/Client/Video.py
+++ Code/Client/Video.py
@@ -130,10 +130,75 @@ class VideoStreaming:
             # Initialize frame rate calculation
             frame_rate_calc = 15
 
+            """
+            frame_rgb = cv2.cvtColor(res, cv2.COLOR_BGR2RGB)
+
+            cv2.imwrite('frameRGB.jpg', frame_rgb) #the inverted colors???
+
+            frame_resized = cv2.resize(frame_rgb, (width, height))
+
+            cv2.imwrite('frameResized.jpg', frame_resized) # resized video for some reason?
+
+            input_data = np.expand_dims(frame_resized, axis=0)
+
+            # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
+            if floating_model:
+                input_data = (np.float32(input_data) - input_mean) / input_std
+
+            # Perform the actual detection by running the model with the image as input
+            interpreter.set_tensor(input_details[0]['index'],input_data)
+            interpreter.invoke()
+
+            # Retrieve detection results
+            boxes = interpreter.get_tensor(output_details[boxes_idx]['index'])[0] # Bounding box coordinates of detected objects
+            classes = interpreter.get_tensor(output_details[classes_idx]['index'])[0] # Class index of detected objects
+            scores = interpreter.get_tensor(output_details[scores_idx]['index'])[0] # Confidence of detected objects
+
+            max_score = 0
+            max_index = 0
 
 
+            #sports ball should be 36
 
+            # Loop over all detections and draw detection box if confidence is above minimum threshold
+            for i in range(len(scores)):
+                # Found desired object with decent confidence
+                if ( (scores[i] > max_score) and (scores[i] > min_conf_threshold) and (scores[i] <= 1.0) and ((labels[int(classes[i])] == "sports ball") or labels[int(classes[i])] == "apple" or labels[int(classes[i])] == "bowl")):
+                    # Get bounding box coordinates and draw box
+                    # Interpreter can return coordinates that are outside of image dimensions, need to force them to be within image using max() and min()
+                    ymin = int(max(1,(boxes[i][0] * imH)))
+                    xmin = int(max(1,(boxes[i][1] * imW)))
+                    ymax = int(min(imH,(boxes[i][2] * imH)))
+                    xmax = int(min(imW,(boxes[i][3] * imW)))
+                    
+                    # Draw label
+                    object_name = (self.color + " ball") # Look up object name from "labels" array using class index
+                    label = '%s: %d%%' % (object_name, int(scores[i]*100)) # Example: 'person: 72%'
+                    labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2) # Get font size
+                    label_ymin = max(ymin, labelSize[1] + 10) # Make sure not to draw label too close to top of window
+                    cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), (10, 255, 0), 2)
+                    cv2.rectangle(frame, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED) # Draw white box to put label text in
+                    cv2.putText(frame, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2) # Draw label text
 
+                    # Record current max
+                    max_score = scores[i]
+                    max_index = i
+
+            if (max_index != 0):
+                ymin = int(max(1,(boxes[max_index][0] * imH)))
+                xmin = int(max(1,(boxes[max_index][1] * imW)))
+                ymax = int(min(imH,(boxes[max_index][2] * imH)))
+                xmax = int(min(imW,(boxes[max_index][3] * imW)))
+                self.face_x = float(xmin+xmax/2)
+                self.face_y = float(ymin+ymax/2)
+
+            else:
+                Stop = '#0#0#0#0\n'
+                self.sendData(cmd.CMD_MOTOR+Stop)
+                self.sendData(cmd.CMD_MODE+"#"+'six'+"#"+'-2'+"\n")
+            """
+            
+            """"""
             frame_rgb = cv2.cvtColor(res_red, cv2.COLOR_BGR2RGB)
 
             cv2.imwrite('frameRGB.jpg', frame_rgb) #the inverted colors???
@@ -344,83 +409,100 @@ class VideoStreaming:
 
 
 
-            # frame_rgb = cv2.cvtColor(res_yellow, cv2.COLOR_BGR2RGB)
+            frame_rgb = cv2.cvtColor(res_yellow, cv2.COLOR_BGR2RGB)
 
-            # cv2.imwrite('frameRGB.jpg', frame_rgb) #the inverted colors???
+            cv2.imwrite('frameRGB.jpg', frame_rgb) #the inverted colors???
 
-            # frame_resized = cv2.resize(frame_rgb, (width, height))
+            frame_resized = cv2.resize(frame_rgb, (width, height))
 
-            # cv2.imwrite('frameResized.jpg', frame_resized) # resized video for some reason?
+            cv2.imwrite('frameResized.jpg', frame_resized) # resized video for some reason?
 
-            # input_data = np.expand_dims(frame_resized, axis=0)
+            input_data = np.expand_dims(frame_resized, axis=0)
 
-            # # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
-            # if floating_model:
-            #     input_data = (np.float32(input_data) - input_mean) / input_std
+            # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
+            if floating_model:
+                input_data = (np.float32(input_data) - input_mean) / input_std
 
-            # # Perform the actual detection by running the model with the image as input
-            # interpreter.set_tensor(input_details[0]['index'],input_data)
-            # interpreter.invoke()
+            # Perform the actual detection by running the model with the image as input
+            interpreter.set_tensor(input_details[0]['index'],input_data)
+            interpreter.invoke()
 
-            # # Retrieve detection results
-            # boxes = interpreter.get_tensor(output_details[boxes_idx]['index'])[0] # Bounding box coordinates of detected objects
-            # classes = interpreter.get_tensor(output_details[classes_idx]['index'])[0] # Class index of detected objects
-            # scores = interpreter.get_tensor(output_details[scores_idx]['index'])[0] # Confidence of detected objects
+            # Retrieve detection results
+            boxes = interpreter.get_tensor(output_details[boxes_idx]['index'])[0] # Bounding box coordinates of detected objects
+            classes = interpreter.get_tensor(output_details[classes_idx]['index'])[0] # Class index of detected objects
+            scores = interpreter.get_tensor(output_details[scores_idx]['index'])[0] # Confidence of detected objects
 
-            # max_score = 0
-            # max_index = 0
+            max_score = 0
+            max_index = 0
 
 
-            # #sports ball should be 36
+            #sports ball should be 36
 
-            # # Loop over all detections and draw detection box if confidence is above minimum threshold
-            # for i in range(len(scores)):
-            #     # Found desired object with decent confidence
-            #     if ( (scores[i] > min_conf_threshold) and (scores[i] <= 1.0) and ((labels[int(classes[i])] == "sports ball") or labels[int(classes[i])] == "apple" or labels[int(classes[i])] == "bowl")):
-            #         # Get bounding box coordinates and draw box
-            #         # Interpreter can return coordinates that are outside of image dimensions, need to force them to be within image using max() and min()
-            #         ymin = int(max(1,(boxes[i][0] * imH)))
-            #         xmin = int(max(1,(boxes[i][1] * imW)))
-            #         ymax = int(min(imH,(boxes[i][2] * imH)))
-            #         xmax = int(min(imW,(boxes[i][3] * imW)))
+            # Loop over all detections and draw detection box if confidence is above minimum threshold
+            for i in range(len(scores)):
+                # Found desired object with decent confidence
+                if ( (scores[i] > min_conf_threshold) and (scores[i] <= 1.0) and ((labels[int(classes[i])] == "sports ball") or labels[int(classes[i])] == "apple" or labels[int(classes[i])] == "bowl")):
+                    # Get bounding box coordinates and draw box
+                    # Interpreter can return coordinates that are outside of image dimensions, need to force them to be within image using max() and min()
+                    ymin = int(max(1,(boxes[i][0] * imH)))
+                    xmin = int(max(1,(boxes[i][1] * imW)))
+                    ymax = int(min(imH,(boxes[i][2] * imH)))
+                    xmax = int(min(imW,(boxes[i][3] * imW)))
                     
-            #         # Draw label
-            #         object_name = labels[int(classes[i])] # Look up object name from "labels" array using class index
-            #         label = '%s: %d%%' % ("yellow ball", int(scores[i]*100)) # Example: 'person: 72%'
-            #         labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2) # Get font size
-            #         label_ymin = max(ymin, labelSize[1] + 10) # Make sure not to draw label too close to top of window
-            #         cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), (10, 255, 0), 2)
-            #         cv2.rectangle(frame, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED) # Draw white box to put label text in
-            #         cv2.putText(frame, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2) # Draw label text
+                    # Draw label
+                    object_name = labels[int(classes[i])] # Look up object name from "labels" array using class index
+                    label = '%s: %d%%' % ("yellow ball", int(scores[i]*100)) # Example: 'person: 72%'
+                    labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2) # Get font size
+                    label_ymin = max(ymin, labelSize[1] + 10) # Make sure not to draw label too close to top of window
+                    cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), (10, 255, 0), 2)
+                    cv2.rectangle(frame, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED) # Draw white box to put label text in
+                    cv2.putText(frame, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2) # Draw label text
 
-            #         # Record current max
-            #         max_score = scores[i]
-            #         max_index = i
+                    # Record current max
+                    max_score = scores[i]
+                    max_index = i
 
-            # if (max_index != 0):
-            #     ymin = int(max(1,(boxes[max_index][0] * imH)))
-            #     xmin = int(max(1,(boxes[max_index][1] * imW)))
-            #     ymax = int(min(imH,(boxes[max_index][2] * imH)))
-            #     xmax = int(min(imW,(boxes[max_index][3] * imW)))
-            #     self.face_x = float(xmin+xmax/2)
-            #     self.face_y = float(ymin+ymax/2)
+            if (max_index != 0):
+                ymin = int(max(1,(boxes[max_index][0] * imH)))
+                xmin = int(max(1,(boxes[max_index][1] * imW)))
+                ymax = int(min(imH,(boxes[max_index][2] * imH)))
+                xmax = int(min(imW,(boxes[max_index][3] * imW)))
+                self.face_x = float(xmin+xmax/2)
+                self.face_y = float(ymin+ymax/2)
 
-            # else:
-            #     Stop = '#0#0#0#0\n'
-            #     self.sendData(cmd.CMD_MOTOR+Stop)
-            #     self.sendData(cmd.CMD_MODE+"#"+'six'+"#"+'-2'+"\n")
-
-
-
-
+            else:
+                Stop = '#0#0#0#0\n'
+                self.sendData(cmd.CMD_MOTOR+Stop)
+                self.sendData(cmd.CMD_MODE+"#"+'six'+"#"+'-2'+"\n")
+            """"""
 
             # Draw framerate in corner of frame
             cv2.putText(frame,'FPS: {0:.2f}'.format(frame_rate_calc),(30,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,0),2,cv2.LINE_AA)
         
         cv2.imwrite('video.jpg', frame)
     
-    """
-    def color_detect(self,img): # Note that RGB is backwards in this function. Instead, it is BGR, so invert where you put your values.
+    # def color_detect(self,img): # Note that RGB is backwards in this function. Instead, it is BGR, so invert where you put your values.
+    #     try:
+    #         hvsframe = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    #         hsc_dic = {"red":(np.array([136, 87, 111], np.uint8),np.array([180, 255, 255], np.uint8)),
+    #                    "blue":(np.array([94, 80, 2], np.uint8),np.array([120, 255, 255], np.uint8)),
+    #                    "green":(np.array([45, 100, 72], np.uint8),np.array([90, 255, 255], np.uint8)),
+    #                    "yellow":(np.array([25, 50, 70], np.uint8),np.array([35, 255, 255], np.uint8))}
+
+    #         limits = hsc_dic[color.lower()]
+
+    #         mask = cv2.inRange(hvsframe, limits[0], limits[1])
+
+    #         res = cv2.bitwise_and(img,img, mask = mask)
+
+    #         self.object_detect(img, res)
+
+    #     except: 
+    #         cv2.imwrite('video.jpg',img)
+    #         pass
+
+        """
         # Convert the img in 
         # BGR(RGB color space) to
         # HSV(hue-saturation-value)
@@ -522,7 +604,7 @@ class VideoStreaming:
         cv2.imwrite('green.jpg',res_green)
         cv2.imwrite('blue.jpg',res_blue)
         cv2.imwrite('yellow.jpg',res_yellow)
-    """
+        """
 
     def streaming(self,ip):
         stream_bytes = b' '
--- Code/Server/arena.py
+++ Code/Server/arena.py
@@ -31,16 +31,122 @@ class Arena:
         # self.infrared=Line_Tracking()
         # self.led=Led()
         self.motor=Motor()
+        """
+        Motor Legend:
+            M - Motor
+            F - Front
+            B - Back
+            L - Left
+            R - Right
+        PWM.setMotorModel(M1, M2, M4, M3)
+        M1, M2, M4, M3   =   FL, BL, FR, BR
+        """
+
         self.servo=Servo()
+
+        self.io = False # Inside = False ; Outside = True
+        self.Ls = False
+        self.Ms = False
+        self.Rs = False
+        self.red    = False
+        self.green  = False
+        self.blue   = False
+        self.yellow = False
 
         self.IRL = 14 # Left   (+4)
         self.IRM = 15 # Middle (+2)
         self.IRR = 23 # Right  (+1)
-        # IR = [IRL, IRM, IRR] # If this doesn't work, just manually setup each
         GPIO.setmode(GPIO.BCM)
-        # GPIO.setup(IR, GPIO.IN)
         GPIO.setup(self.IRL, GPIO.IN)
         GPIO.setup(self.IRM, GPIO.IN)
         GPIO.setup(self.IRR, GPIO.IN)
     def run(self):
+        self.motor.setMotorModel(0,0,0,0)
+        
+        moving = True # 'tracking' can be renamed to 'patrol'; either way, this is supposed to prevent the car from leaving the arena; this boolean is 'moving' for when the patrolling occurs
+        while moving:
+            """
+            Sensors | Truth | Sum | Action
+            lmr     | 000   |  0  | nul
+            lmR     | 001   |  1  | Turn Left - Soft
+            lMr     | 010   |  2  | U-Turn
+            lMR     | 011   |  3  | Turn Left - Hard
+            Lmr     | 100   |  4  | Turn Right - Soft
+            LmR     | 101   |  5  | U-Turn
+            LMr     | 110   |  6  | Turn Right - Hard
+            LMR     | 111   |  7  | U-Turn
+            """
+            LMR=0x00 # Detects Black
+            if GPIO.input(self.IRR)==self.io: # Right Sensor
+                LMR=(LMR | 1)
+            #     Rs = True
+            # else:
+            #     Rs = False
+            if GPIO.input(self.IRM)==self.io: # Middle Sensor
+                LMR=(LMR | 2)
+            #     Ms = True
+            # else:
+            #     Ms = False
+            if GPIO.input(self.IRL)==self.io: # Left Sensor
+                LMR=(LMR | 4)
+            #     Ls = True
+            # else:
+            #     Ls = False
+
+            if   LMR==0: # nul
+                pass
+            elif LMR==4: # Turn Right - Soft
+                self.motor.setMotorModel(0,0,0,0)
+                time.sleep(1)
+                self.motor.setMotorModel(1500,1500,-1500,-1500)
+                time.sleep(0.5)
+                self.motor.setMotorModel(0,0,0,0)
+
+                moving = False
+            elif LMR==6: # Turn Right - Hard
+                self.motor.setMotorModel(0,0,0,0)
+                time.sleep(1)
+                self.motor.setMotorModel(1500,1500,-1500,-1500)
+                time.sleep(1)
+                self.motor.setMotorModel(0,0,0,0)
+
+                moving = False
+            elif LMR==2 or LMR==5 or LMR==7: # U-Turn
+                self.motor.setMotorModel(0,0,0,0)
+                time.sleep(1)
+                self.motor.setMotorModel(-1500,-1500,-1500,-1500)
+                time.sleep(1)
+                self.motor.setMotorModel(0,0,0,0)
+                time.sleep(1)
+                self.motor.setMotorModel(1500,1500,-1500,-1500)
+                time.sleep(2)
+                self.motor.setMotorModel(0,0,0,0)
+
+                moving = False
+            elif LMR==3: # Turn Left - Hard
+                self.motor.setMotorModel(0,0,0,0)
+                time.sleep(1)
+                self.motor.setMotorModel(-1500,-1500,1500,1500)
+                time.sleep(1)
+                self.motor.setMotorModel(0,0,0,0)
+
+                moving = False
+            elif LMR==1: # Turn Left - Soft
+                self.motor.setMotorModel(0,0,0,0)
+                time.sleep(1)
+                self.motor.setMotorModel(-1500,-1500,1500,1500)
+                time.sleep(0.5)
+                self.motor.setMotorModel(0,0,0,0)
+
+                moving = False
+            
+            print('placeholder')
+
+arena=Arena()
+# Main program logic follows
+if __name__ == '__main__':
+    print ('Program is starting ... ')
+    try:
+        arena.run()
+    except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program  will be  executed.
         PWM.setMotorModel(0,0,0,0)
